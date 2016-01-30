cda9c920c706a7c367cd727242d7a9ebfdc350cd
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2016-01-30 15:41:18 -0500

    making boilerplate generation happen just 1x

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ var path = require('path')
 var inquirer = require('inquirer')
 var wordwrap = require('wordwrap')
 var assert = require('assertf')
+var fs = require('fs');
 
 module.exports = function(options) {
   // Prevent automatic close of stdin, so that input is possible after showing the problem description.
-  options.autoclose = false
+  //options.autoclose = false
   var shop = adventure(options)
+  var execute = shop.execute;
+  //record firstrun
+  shop.files.firstrun = path.join(shop.datadir, 'firstrun.json');
   shop.on('pass', function() {
     console.log('Type `' + options.name+ '` to show the menu.\n')
   })
@@ -35,26 +39,35 @@ module.exports = function(options) {
     assert(exercise.name, 'exercise needs name')
     assert(exercise.problem, 'exercise %s needs problem', exercise.name)
     assert(exercise.solution, 'exercise %s needs solution', exercise.name)
-    assert(exercise.boilerplate, 'exercise %s needs boilerplate', exercise.name)
     assert(exercise.verify, 'exercise %s needs verify', exercise.name)
   })
 
   exercises.forEach(function(exercise) {
     shop.add(exercise.name, function() {
-      setTimeout(function() {
-        setup(exercises)
-      }, 10)
       return exercise;
     })
   })
+
+  shop.execute = function(args){
+    var beenRunPath = path.join(this.datadir, 'has_been_run');
+    fs.stat(beenRunPath,function(err, stats){
+      if(err){ //first run, prompt for boilerplate
+        fs.writeFile(beenRunPath, 'yep',function(){});
+        setup(exercises, function(){
+          execute.call(shop,[].slice.apply(args));
+        });
+      }else{
+        execute.call(shop,[].slice.apply(args));
+      }
+    });
+  };
+
   return shop
 }
 
-function setup(exercises) {
+function setup(exercises, done) {
   var boilerPlateRequired = exercises.filter(function(exercise) {
     return exercise.boilerplate
-  }).filter(function(exercise) {
-    return !fs.existsSync(process.cwd() + '/' + exercise.cleanName)
   }).map(function(exercise) {
     return {
       boilerplate: exercise.boilerplate,
@@ -64,20 +77,21 @@ function setup(exercises) {
   })
 
   if (boilerPlateRequired.length) {
-    console.log('\n' + process.cwd() + '\n')
     inquirer.prompt([{
       'type': 'confirm',
       'name': 'ok',
       'default': true,
       'message': wordwrap(4, 80)(
-        "We're about to populate the above directory with some files needed for the exercises. " +
-        "If they've already been created then don't worry, " +
-        "they won't be replaced. Continue?"
+        "Welcome! These exercises have some boilerplate files/directories included to help " +
+        "you get started with each exercise. They are helpful but not necessary. " +
+        "If you'd like to have them created now (in the current directory) enter 'Y'. " +
+        "If you'd like to start without boilerplate files enter 'n'\n"
       ).replace(/^\s+/, '')
     }], function(result) {
-      if (!result.ok) return
+      if (!result.ok) return done();
       boilerPlateRequired.forEach(function(item) {
         try {
+          console.log('Creating directory ' + item.dir);
           fs.mkdirSync(item.dir)
         } catch(e) {
           // probably already exists
@@ -85,7 +99,16 @@ function setup(exercises) {
         fs.writeFileSync(item.dir + '/Readme.md', item.problem)
         fs.writeFileSync(item.dir + '/index.js', item.boilerplate)
       })
-    })
+      inquirer.prompt([{
+        'type': 'confirm',
+        'name': 'ok',
+        'default': true,
+        'message': "Boilerplate files created! Launch exercises? (press enter for yes)"
+        }], function(res){
+          if(res.ok) return done();
+          process.exit(0);
+      });
+    });
   }
 }
 
